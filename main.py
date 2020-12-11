5a123e0bb1cc168310fddcb282385f5e2f684224
Jan Christian Blaise Cruz <jcblaise@Blaises-MacBook-Pro.local>
2020-12-11 12:00:38 +0800

    Added delay

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def main():
 			print("Folder contents have changed!")
 			print("Added: " + str(set(ncontents) - set(contents)))
 			contents = ncontents
+			time.sleep(args.sleep)
+			print("Command executing...")
 			os.system(args.command)
 			print("Command executed. Resume watching.")
 			
